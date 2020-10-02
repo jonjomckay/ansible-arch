a24938652cb70643aa8915add341b429a9184235
Jonjo McKay <jonjo@jonjomckay.com>
2020-10-03 00:45:16 +0100

    Now logging Sway output to journald

--- roles/sway/files/sway.zsh
+++ roles/sway/files/sway.zsh
@@ -1,4 +1,4 @@
 # Start sway when tty1 starts
 if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
-  _JAVA_AWT_WM_NONREPARENTING=1 exec sway
+  _JAVA_AWT_WM_NONREPARENTING=1 exec systemd-cat --identifier=sway sway
 fi

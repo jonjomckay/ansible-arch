19cb61c5fd6aa674b81510a947f7538d5ccbfe43
Jonjo McKay <jonjo@jonjomckay.com>
2020-10-26 20:56:10 +0000

    Made keyring work again with ZSH autologin

--- roles/sway/files/sway.zsh
+++ roles/sway/files/sway.zsh
@@ -1,4 +1,7 @@
 # Start sway when tty1 starts
 if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
+  eval $(gnome-keyring-daemon --start)
+  export SSH_AUTH_SOCK
+
   exec systemd-cat --identifier=sway sway
 fi 

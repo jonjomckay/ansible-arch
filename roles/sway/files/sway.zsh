b36ca73d8c8ab8fb5f9ff1129489597d0bf0409e
Jonjo McKay <jonjo@jonjomckay.com>
2020-11-19 13:23:19 +0000

    Added polkit to Sway startup

--- roles/sway/files/sway.zsh
+++ roles/sway/files/sway.zsh
@@ -1,7 +1,12 @@
 # Start sway when tty1 starts
 if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
+  # Start GNOME Keyring for secrets
   eval $(gnome-keyring-daemon --start)
   export SSH_AUTH_SOCK
 
+  # Start Polkit for system privilege magic
+  exec /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 
+
+  # Start Sway
   exec systemd-cat --identifier=sway sway
 fi 

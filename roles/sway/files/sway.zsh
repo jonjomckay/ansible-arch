a9689acef84f240ad6cf94789a35b6a1ce134644
Jonjo McKay <jonjo@jonjomckay.com>
2020-11-19 13:40:43 +0000

    Fixed Polkit not starting sometimes

--- roles/sway/files/sway.zsh
+++ roles/sway/files/sway.zsh
@@ -4,9 +4,6 @@ if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
   eval $(gnome-keyring-daemon --start)
   export SSH_AUTH_SOCK
 
-  # Start Polkit for system privilege magic
-  exec /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 
-
   # Start Sway
   exec systemd-cat --identifier=sway sway
 fi 

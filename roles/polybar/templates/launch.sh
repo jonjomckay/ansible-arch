e4df8d459b435d81206eb19cac74681444bd7edf
Jonjo McKay <jonjo@jonjomckay.com>
2021-02-10 12:04:23 +0000

    Now displaying Polybar on all monitors

--- roles/polybar/templates/launch.sh
+++ roles/polybar/templates/launch.sh
@@ -7,6 +7,8 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar
-polybar --config=~/.config/polybar/config.ini mybar &
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload --config=~/.config/polybar/config.ini mybar &
+done
 
 echo "Polybar launched..."

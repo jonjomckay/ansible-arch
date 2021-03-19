3f452f8d8d6c7dd4f5df06eb9b27970bdda64cc4
Jonjo McKay <jonjo@jonjomckay.com>
2021-03-19 13:13:23 +0000

    Now using standalone SSH agent

--- roles/sway/files/window-manager.zsh
+++ roles/sway/files/window-manager.zsh
@@ -1,9 +1,5 @@
 # Start sway when tty1 starts
 if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
-  # Start GNOME Keyring for secrets
-  eval $(gnome-keyring-daemon --start)
-  export SSH_AUTH_SOCK
-
   # Start Sway
   exec systemd-cat --identifier=sway sway
 fi 
